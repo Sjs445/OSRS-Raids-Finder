8e7b47da8e5abef3d046ddce66276605a9887c48
Mark Hardebeck <mhardebeck@gmail.com>
2020-08-21 11:22:59 -0700

    Mark

--- client/src/components/modal/createPartyModal.js
+++ client/src/components/modal/createPartyModal.js
@@ -1,104 +1,126 @@
-import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
-import { connect } from 'react-redux';
-import { addParty } from '../../actions/partyActions';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
+import { connect } from "react-redux";
+import { addParty } from "../../actions/partyActions";
+import PropTypes from "prop-types";
 
 class CreatePartyModal extends Component {
-    state = {
-        modal: false,
-        raidType: '',
-        clanChat: '',
-        partyLeader: ''
-    }
+  state = {
+    modal: false,
+    raidType: "",
+    clanChat: "",
+    partyLeader: "",
+  };
 
-    toggle = () => {
-        this.setState({
-            modal: !this.state.modal
-        });
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const newParty = {
+      raidType: this.state.raidType,
+      clanChat: this.state.clanChat,
+      partyLeader: this.props.user.rsn,
+      userid: this.props.user._id,
     };
+    console.log(this.state.raidType);
 
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
-    };
+    //  Add party via addParty action
+    this.props.addParty(newParty);
 
-    onSubmit = (e) => {
-        e.preventDefault();
+    //  Close modal
+    this.toggle();
+  };
 
-        const newParty = {
-            raidType: this.state.raidType,
-            clanChat: this.state.clanChat,
-            partyLeader: this.props.user.rsn,
-            userid: this.props.user._id
-        }
+  render() {
+    return (
+      <div>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={this.toggle}
+        >
+          Add Party
+        </Button>
 
-        //  Add party via addParty action
-        this.props.addParty(newParty);
-
-        //  Close modal
-        this.toggle();
-    }
-
-    render() {
-        return(
-            <div>
-                <Button
-                    color="dark"
-                    style={{marginBottom: '2rem'}}
-                    onClick={this.toggle}
-                >Add Party</Button>
-
-                <Modal
-                    isOpen={this.state.modal}
-                    toggle={this.toggle}
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>New Party</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="raidType">Raid Type</Label>
+                <br />
+                <select
+                  // type="text"
+                  // name="raidType"
+                  // id="raidType"
+                  // placeholder="Chambers of Xeric"
+                  onChange={this.onChange.bind(this)}
+                  name="raidType"
+                  id="raidType"
                 >
-                    <ModalHeader toggle={this.toggle}>New Party</ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.onSubmit}>
-                            <FormGroup>
-                                <Label for="raidType">Raid Type</Label>
-                                <Input 
-                                type="text"
-                                name="raidType"
-                                id="raidType"
-                                placeholder="Chambers of Xeric"
-                                onChange={this.onChange}
-                                />
-                                <Label for="clanChat">Clan Chat</Label>
-                                <Input
-                                type="text"
-                                name="clanChat"
-                                id="clanChat"
-                                placeholder={this.props.user.rsn}
-                                onChange={this.onChange}
-                                />
-                                <Label for="partyLeader">Party Leader</Label>
-                                <Input
-                                type="text"
-                                id="partyLeader"
-                                placeholder={this.props.user.rsn}
-                                value={this.props.user.rsn}
-                                disabled />
-                                <Button color="dark" type="submit"
-                                style={{marginTop: '2rem'}} block
-                                >Create Party</Button>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-                </Modal>
-            </div>
-        );
-    }
+                  <option>Chambers of Xeric</option>
+                  <option>Theatre of Blood</option>
+                </select>
+                <br />
+                <Label for="clanChat">Clan Chat</Label>
+                <Input
+                  type="text"
+                  name="clanChat"
+                  id="clanChat"
+                  placeholder={this.props.user.rsn}
+                  onChange={this.onChange}
+                />
+                <Label for="partyLeader">Party Leader</Label>
+                <Input
+                  type="text"
+                  id="partyLeader"
+                  placeholder={this.props.user.rsn}
+                  value={this.props.user.rsn}
+                  disabled
+                />
+                <Button
+                  color="dark"
+                  type="submit"
+                  style={{ marginTop: "2rem" }}
+                  block
+                >
+                  Create Party
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 CreatePartyModal.propTypes = {
-    party: PropTypes.object.isRequired,
-    user: PropTypes.object.isRequired
+  party: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    party: state.party,
-    user: state.auth.user
+const mapStateToProps = (state) => ({
+  party: state.party,
+  user: state.auth.user,
 });
 
 export default connect(mapStateToProps, { addParty })(CreatePartyModal);
