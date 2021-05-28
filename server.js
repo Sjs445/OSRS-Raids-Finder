bc0489d79fdca44fa905f4b69c82aca62bc8fbdf
sjs445 <sjs445@csu.fullerton.edu>
2021-05-28 09:37:15 -0700

    Updating node packages, fixes 3 vulnerabilities

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use("/api/party", party);
 if(process.env.NODE_ENV === "production") {
 	//	Set Static folder
 	app.use(express.static("client/build"));
-	app.get("*", (req, res) => {
+	app.get("/*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
